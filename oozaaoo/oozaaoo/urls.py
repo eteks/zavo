0987dfcafabcbacf47c00e16131a92af54a50075
anand <anand@etekchnoservices.com>
2018-01-18 15:47:39 +0530

    update import settings

--- oozaaoo/oozaaoo/urls.py
+++ oozaaoo/oozaaoo/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 from django.conf.urls.static import static,serve
+from django.conf import settings
 
 urlpatterns = [
 	url(r'^jet/', include('jet.urls', 'jet')),
